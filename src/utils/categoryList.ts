98ffd5f750b851681a19d2f6273c57e29d146475
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-07-16 11:17:31 -0300

    feat: add helper for adding image ulr

--- src/utils/categoryList.ts
+++ src/utils/categoryList.ts
@@ -1,4 +1,4 @@
-type Skill = {
+export type Skill = {
     name: string;
     descrition: string;
     documentation: string | null;
---
+++ src/utils/imagesUrl.ts
@@ -0,0 +1,62 @@
+
+import backend from "../assets/images/category/backend.png";
+import frontend from "../assets/images/category/frontend.png";
+import languages from "../assets/images/category/languages.png";
+import libsAndFrameworks from "../assets/images/category/frameworks.png";
+import tools from "../assets/images/category/tools.png";
+
+
+import css from "../assets/images/skills/css.png";
+import express from "../assets/images/skills/express.png";
+import git from "../assets/images/skills/git.png";
+import github from "../assets/images/skills/github.png";
+import html from "../assets/images/skills/html.png";
+import jest from "../assets/images/skills/jest.png";
+import javascript from "../assets/images/skills/javascript.png";
+import knexjs from "../assets/images/skills/knexjs.png";
+import linux from "../assets/images/skills/linux.png";
+import next from "../assets/images/skills/next.png";
+import nodejs from "../assets/images/skills/nodejs.png";
+import prisma from "../assets/images/skills/prisma.png";
+import react from "../assets/images/skills/react.png";
+import sass from "../assets/images/skills/sass.png";
+import sequelize from "../assets/images/skills/sequelize.png";
+import styledcomponents from "../assets/images/skills/styledcomponents.png";
+import tailwind from "../assets/images/skills/tailwind.png";
+import typescript from "../assets/images/skills/typescript.png";
+import vscode from "../assets/images/skills/vscode.png";
+
+
+
+
+
+
+
+
+export {
+    backend,
+    frontend,
+    languages,
+    libsAndFrameworks,
+    tools,
+    css,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    jest,
+    knexjs,
+    linux,
+    next,
+    nodejs,
+    prisma,
+    react,
+    sass,
+    sequelize,
+    styledcomponents,
+    tailwind,
+    typescript,
+    vscode
+
+}
