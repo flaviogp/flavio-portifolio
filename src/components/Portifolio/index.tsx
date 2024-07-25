dccbe4aa805b82e4200162f4f4ecd6e17572680e
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-07-25 11:58:35 -0300

    feat: add connection to github api

--- src/components/Portifolio/index.tsx
+++ src/components/Portifolio/index.tsx
@@ -1,12 +1,20 @@
+import { useGithubApi } from "../../hooks/useGithubApi";
 import Container from "../Container";
 
 const Portifolio = () => {
+  const repositories = useGithubApi("flaviogp");
   return (
     <Container bg={"secondary"}>
       <div className="flex flex-col space-y-4 items-center text-center">
         <h2 className="text-secondary-text-color font-semibold uppercase text-sm">
           Portifolio
         </h2>
+        <div>
+          {repositories.map((repo) => (
+            <h2 key={repo.id}>{repo.name}</h2>
+          ))}
+          <p></p>
+        </div>
       </div>
     </Container>
   );
---
+++ src/utils/formatRepositories.ts
@@ -0,0 +1,23 @@
+import { githubRepository, repository } from "./types"
+
+const formatRepositories = (data: githubRepository[] ): repository[] => {
+
+    const formatData: repository[] = []
+
+    data.map(repo => {
+        const formatRepo = {
+            id: repo.id,
+            name: repo.name,
+            html_url: repo.html_url,
+            created_at: repo.created_at,
+            language: repo.language
+        }
+
+        formatData.push(formatRepo)
+    })
+
+
+    return formatData
+} 
+
+export default formatRepositories
